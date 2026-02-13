5cb244add096dd684636555ce26411d04df5e0a2
qhorgues <quentin.horgues@outlook.fr>
2026-02-13 15:14:37 +0100

    add 2 models

---
+++ app/Models/Logs/LogsItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Logs;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class LogsItem extends Model
+{
+    public function log(): MorphOne
+    {
+        return $this->morphOne(Logs::class, "logable");
+    }
+}
