92ad5abf9eb150d65e3b83aa6ccf81cbb53be3ff
qhorgues <quentin.horgues@outlook.fr>
2026-02-13 14:46:06 +0100

    Model Logs

--- app/Models/Logs.php
+++ app/Models/Logs.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+#[MorphTo("logable")]
 class Logs extends Model
 {
-    public function logable(): MorphTo
-    {
-        return $this->morphTo();
-    }
 }
--- app/Models/LogsVisitor.php
+++ app/Models/LogsVisitor.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
+#[MorphOne(Logs::class, "logable")]
 class LogsVisitor extends Model
 {
-    public function log(): MorphOne
-    {
-        return $this->morphOne(Logs::class, "logable");
-    }
 }
