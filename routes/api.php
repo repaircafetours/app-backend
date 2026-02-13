7e75948241bd15e2d63f97e0b24f45dc63869ee3
qhorgues <quentin.horgues@outlook.fr>
2026-02-13 14:46:11 +0100

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\VolunteerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/volunteer', [VolunteerController::class, 'index']);
+
+Route::get('/volunteer/{id}', [VolunteerController::class, 'show']);
