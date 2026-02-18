98dfc6348858a352350b62418b2c89495fe8d910
Younes <younes.chahid@etu.univ-tours.fr>
2026-02-18 12:27:43 +0100

    [enhancement] added delete route

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::get("/v1/visitors/{visitor}", [VisitorController::class, 'show']);
 Route::get("/v1/visitors", [VisitorController::class, 'index']);
 Route::post("/v1/visitors", [VisitorController::class, 'store']);
 Route::patch("/v1/visitors/{visitor}", [VisitorController::class, 'update']);
+Route::delete("/v1/visitors/{visitor}", [VisitorController::class, 'destroy']);
