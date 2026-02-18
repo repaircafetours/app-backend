85a95a2656515b0b1a1b9fb35ba9fe907c767d1a
Younes <younes.chahid@etu.univ-tours.fr>
2026-02-18 12:00:27 +0100

    [enhancement] made the email field unique

--- database/migrations/2026_01_13_150106_create_visitors_table.php
+++ database/migrations/2026_01_13_150106_create_visitors_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
             $table->string('email')
-                ->index();
+                ->unique();
             $table->string('title');
             $table->string('name');
             $table->string('surname');
